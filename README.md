cffd905d2cda562c24f6ed7245119360b88f8b6e
erpost <25180070+erpost@users.noreply.github.com>
2019-12-30 13:34:12 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Create Service Principal key: az ad sp create-for-rbac --role reader --name [name] --sdk-auth > filename.json
+Create Service Principal key: az ad sp create-for-rbac --role reader --name [name] --sdk-auth > ~/.azure/sp_keys/readonly.json
--- list_resources_in_resource_group.py
+++ list_resources_in_resource_group.py
@@ -1,7 +1,6 @@
 from azure.common.client_factory import get_client_from_auth_file
 from azure.mgmt.resource import ResourceManagementClient
 import os
-from pprint import pprint
 
 
 os.environ['AZURE_AUTH_LOCATION'] = os.path.expanduser('~/.azure/sp_keys/readonly.json')
@@ -9,6 +8,7 @@ client = get_client_from_auth_file(ResourceManagementClient)
 
 for rg in client.resource_groups.list():
     for item in client.resources.list_by_resource_group(rg.name):
+        # print(item)
         print('Resource Group: ', rg.name)
         print('Name: ', item.name)
         print('Type: ', item.type)
