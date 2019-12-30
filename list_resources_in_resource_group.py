c6164f0194f8ffac2e33a60d3664557a8fd61042
erpost <25180070+erpost@users.noreply.github.com>
2019-12-30 13:23:08 -0500

    update

--- list_resources_in_resource_group.py
+++ list_resources_in_resource_group.py
@@ -1,6 +1,7 @@
 from azure.common.client_factory import get_client_from_auth_file
 from azure.mgmt.resource import ResourceManagementClient
 import os
+from pprint import pprint
 
 
 os.environ['AZURE_AUTH_LOCATION'] = os.path.expanduser('~/.azure/sp_keys/readonly.json')
@@ -8,4 +9,8 @@ client = get_client_from_auth_file(ResourceManagementClient)
 
 for rg in client.resource_groups.list():
     for item in client.resources.list_by_resource_group(rg.name):
-        print(rg.name, ':', item)
+        print('Resource Group: ', rg.name)
+        print('Name: ', item.name)
+        print('Type: ', item.type)
+        print('Location: ', item.location)
+        print('\n')
